129c34ffbd7f903821f83a0d66a4a54a8601eac2
Buddy <203337004+iambuddy01@users.noreply.github.com>
2026-03-12 21:49:20 +0530

    ᴅᴇᴠɴʙᴏᴛs

--- Nexa/__main__.py
+++ Nexa/__main__.py
@@ -1,3 +1,28 @@
-from Nexa.bot import app
+from Nexa.bot import app  
+import Nexa.plugins  
 
-app.run()
+def main():
+
+    print("""
+███╗░░██╗███████╗██╗░░██╗░█████╗░
+████╗░██║██╔════╝╚██╗██╔╝██╔══██╗
+██╔██╗██║█████╗░░░╚███╔╝░███████║
+██║╚████║██╔══╝░░░██╔██╗░██╔══██║
+██║░╚███║███████╗██╔╝╚██╗██║░░██║
+╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝
+""")
+    print("Starting Nexa Bot...")
+    print("🔌 Loading plugins...")
+
+    try:
+        print("🚀 Bot is now running...")
+        app.run()  # Synchronous run
+    except KeyboardInterrupt:
+        print("🛑 Bot stopped manually (Ctrl+C)")
+    except Exception as e:
+        print(f"❌ Bot crashed: {e}")
+    finally:
+        print("⏹ Bot shutdown complete.")
+
+if __name__ == "__main__":
+    main()
