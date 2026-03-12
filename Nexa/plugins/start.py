ce3b58a6c5ce053c77896bdbfa657ec1a7c9dacd
Buddy <203337004+iambuddy01@users.noreply.github.com>
2026-03-12 21:19:39 +0530

    ᴅᴇᴠɴʙᴏᴛs

---
+++ Nexa/plugins/start.py
@@ -0,0 +1,34 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from Nexa.bot import app
+
+START_IMAGE = "https://graph.org/file/6e7c1a1f5c21d5b9a9c3e.jpg"
+
+TEXT = "Welcome To Nexa Bot"
+
+HELP = "https://t.me/yourhelp"
+UPDATE = "https://t.me/yourupdates"
+MAJDURI = "https://t.me/yourrepo"
+SUPPORT = "https://t.me/yoursupport"
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Help", url=HELP),
+                InlineKeyboardButton("Update", url=UPDATE)
+            ],
+            [
+                InlineKeyboardButton("Majduri", url=MAJDURI),
+                InlineKeyboardButton("Support", url=SUPPORT)
+            ]
+        ]
+    )
+
+    await message.reply_photo(
+        photo=START_IMAGE,
+        caption=TEXT,
+        reply_markup=buttons
+)
